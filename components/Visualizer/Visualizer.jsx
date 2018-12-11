51824d0d1c08ebf1d6bfaed38362923c65542c48
Arceso <arcesone@gmail.com>
2018-12-11 13:08:12 +0100

    Added more stilish styles
    WOAH BLACK AND PINK BUD!
    Also a logaritmic-growing level-based background pink noise was added.

--- components/Visualizer/Visualizer.jsx
+++ components/Visualizer/Visualizer.jsx
@@ -1,14 +1,14 @@
-import './Visualizer.styl';
-import { paper } from 'paper';
-import React from 'react'
-import { pathToFileURL } from 'url';
-import { magentaBright } from 'ansi-colors';
+import "./Visualizer.styl";
+import { paper } from "paper";
+import React from "react";
+import { pathToFileURL } from "url";
+import { magentaBright } from "ansi-colors";
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {};
 
     this.canvasRef = React.createRef();
     this.audioRef = React.createRef();
@@ -18,13 +18,12 @@ export default class extends React.Component {
   audioRef;
 
   componentDidMount() {
-    const SONG_PATH = '/static/Media/Sounds/song.mp3',
-      COLORS = ['#03A9F4', '#e91e63'];;
+    const SONG_PATH = "/static/Media/Sounds/song.mp3",
+      COLORS = ["#080808", "#ff0050"];
     let audio = this.audioRef.current,
       audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
       analyser = audioCtx.createAnalyser(),
       canvas = this.canvasRef.current;
-
 
     audio.src = SONG_PATH;
     audio.load();
@@ -56,15 +55,15 @@ export default class extends React.Component {
 
       let lastBisection = view.center.add([100, 0]),
         actualRotation = -DEGS_PER_LINE / 2,
-        arrayCurves = new Array(2).fill().map(() => (new paper.Path())),
+        arrayCurves = new Array(2).fill().map(() => new paper.Path()),
         circle = new paper.Shape.Circle(view.center, 100);
 
-      for (let i = 6; i < BUFFER_LENGTH; i++) {
+      for (let i = 6; i <= BUFFER_LENGTH; i++) {
         if (i % 2) actualRotation += DEGS_PER_LINE;
         arrayCurves[i % 2].moveTo(lastBisection);
         arrayCurves[i % 2].quadraticCurveTo(
           getEndOfLine(DATA_ARRAY[i], actualRotation),
-          lastBisection = getEndOfCurve(actualRotation)
+          (lastBisection = getEndOfCurve(actualRotation))
         );
       }
       for (let i = 0; i < arrayCurves.length; i++) {
@@ -77,17 +76,32 @@ export default class extends React.Component {
         arrayCurves[i].fillColor = COLORS[i];
       }
       arrayCurves[1].style = {
-        shadowColor: 'rgba(21, 21, 21, .3)',
+        shadowColor: "rgba(21, 21, 21, .3)",
         shadowBlur: 2,
         shadowOffset: [0, 5]
-      }
+      };
 
       circle.clone().fillColor = COLORS[0];
 
+      let shadowWidth = DATA_ARRAY[Math.ceil(DATA_ARRAY.length / 2)];
+      if (shadowWidth <= 1) shadowWidth = 1;
+      
+      let circleShadow = new paper.Shape.Circle(view.center, 25 * Math.log(2 * shadowWidth));
+      circleShadow.sendToBack();
+      circleShadow.fillColor = {
+        gradient: {
+            stops: [['#ff0050', shadowWidth / 300], ['transparent', 1]],
+            radial: true
+        },
+        origin: circleShadow.position,
+        destination: circleShadow.bounds.rightCenter
+    };
+      
+
       circle.bringToFront();
       circle.style = {
-        fillColor: '#fff',
-        shadowColor: 'rgba(21, 21, 21, .3)',
+        fillColor: "#fff",
+        shadowColor: "rgba(21, 21, 21, .3)",
         shadowBlur: 2,
         shadowOffset: [0, 5]
       };
@@ -96,8 +110,11 @@ export default class extends React.Component {
       if (DATA_ARRAY.every(level => level > 50)) {
         const x = view.center.x,
           y = view.center.y;
-        
-        circle.position = [getRandomInt(x + 5, x - 5), getRandomInt(y + 5, y - 5)];
+
+        circle.position = [
+          getRandomInt(x + 5, x - 5),
+          getRandomInt(y + 5, y - 5)
+        ];
       }
     }
 
@@ -107,45 +124,58 @@ export default class extends React.Component {
       return view.center.add([
         (size + 100) * TRIGONOMETRY.cos(degrees),
         (size + 100) * TRIGONOMETRY.sin(degrees)
-      ])
+      ]);
     }
 
     function getEndOfCurve(currentAngle) {
       return view.center.add([
         TRIGONOMETRY.cos(bisection(currentAngle)) * 100,
         TRIGONOMETRY.sin(bisection(currentAngle)) * 100
-      ])
+      ]);
     }
 
-    function bisection(currentAngle) { return currentAngle + (DEGS_PER_LINE / 2); }
+    function bisection(currentAngle) {
+      return currentAngle + DEGS_PER_LINE / 2;
+    }
   }
 
   onCanvasClick() {
     const audio = this.audioRef.current;
-    audio.paused ? audio.play(): audio.pause();
+    audio.paused ? audio.play() : audio.pause();
   }
 
   render() {
     return (
       <div>
-        <canvas id="oscilloscope" ref={this.canvasRef} onClick={this.onCanvasClick.bind(this)}/>
+        <canvas
+          id="oscilloscope"
+          ref={this.canvasRef}
+          onClick={this.onCanvasClick.bind(this)}
+        />
         <audio id="audio" controls ref={this.audioRef} />
-        <img src="/static/Media/Images/logo.png" onClick={this.onCanvasClick.bind(this)}/>
+        <img
+          src="/static/Media/Images/logo.png"
+          onClick={this.onCanvasClick.bind(this)}
+        />
       </div>
     );
   }
 }
 
 // Trigonometry101 for dummies:
-function toRadians(degree) { return degree * (Math.PI / 180); }
+function toRadians(degree) {
+  return degree * (Math.PI / 180);
+}
 
-function roundNumber(number, decimals = 5) { return Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals); }
+function roundNumber(number, decimals = 5) {
+  return Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals);
+}
 
 const TRIGONOMETRY = {
   sin: number => roundNumber(Math.sin(toRadians(number))),
   cos: number => roundNumber(Math.cos(toRadians(number)))
-}
+};
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}   
+}
