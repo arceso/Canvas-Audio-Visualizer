8538d760155e9645368162fc404446d520b5c97d
Arceso <arcesone@gmail.com>
2018-12-19 04:09:59 +0100

    Extracted the logic from the react component
    Now the logic is on a separated js file.
    Cleaned and parametrized the logic.

---
+++ components/Visualizer/Visualizer.js
@@ -0,0 +1,153 @@
+import { paper, Size } from "paper";
+
+export default class Visualizer {
+  constructor(media, canvas, cover, colors, circleSize = 100, vibration, normalize, coverVibrates) {
+    this.media = media;
+    this.canvas = canvas;
+    this.cover = cover;
+    this.colors = colors;
+    this.circleSize = circleSize;
+    this.normalize = normalize ? normalize : this.normalize;
+    this.coverVibrates = coverVibrates;
+
+    this.vibration = vibration[0] ? vibration : [vibration, vibration];
+    this.BUFFER_LENGTH = null;
+    this.DATA_ARRAY = null;
+    this.analyser = null;
+    this.DEGS_PER_LINE = 180 / 5;
+  }
+
+  init = () => {
+    //Refresh to current to avoid next.js 
+    this.media = this.media.current;
+    this.canvas = this.canvas.current;
+    this.cover = this.cover.current;
+
+    this.media.load();
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    let mediaCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    this.analyser = mediaCtx.createAnalyser();
+
+    // Sets the source to the media element & adds the analyser.
+    mediaCtx.createMediaElementSource(this.media).connect(this.analyser);
+
+    // links the analyser output to the speakers.
+    this.analyser.connect(mediaCtx.destination);
+
+    //ammount of waveSamples (?)
+    this.analyser.fftSize = 2 ** 5;
+
+    //Maybe gotta look into not loading paper to the whole window.
+    paper.install(window);
+    paper.setup(this.canvas);
+
+    this.BUFFER_LENGTH = this.analyser.frequencyBinCount;
+    this.DATA_ARRAY = new Uint8Array(this.BUFFER_LENGTH);
+
+    this.cover.style.width = this.circleSize * 2 + 'px';
+    this.cover.style.height = this.circleSize * 2 + 'px';
+
+    this.renderFrame();
+  }
+
+  renderFrame = () => {
+
+    requestAnimationFrame(this.renderFrame);
+    this.analyser.getByteFrequencyData(this.DATA_ARRAY);
+    project.activeLayer.removeChildren();
+
+    let lastBisection = view.center.add([this.circleSize, 0]),
+      actualRotation = -this.DEGS_PER_LINE / 2,
+      arrayCurves = new Array(2).fill().map(() => new paper.Path()),
+      circle = new paper.Shape.Circle(view.center, this.circleSize);
+
+    for (let i = 6; i <= this.BUFFER_LENGTH; i++) {
+      if (i % 2) actualRotation += this.DEGS_PER_LINE;
+      arrayCurves[i % 2].moveTo(lastBisection);
+      arrayCurves[i % 2].quadraticCurveTo(
+        this.getEndOfLine(this.normalize(this.DATA_ARRAY[i], !(i % 2)), actualRotation),
+        lastBisection = this.getEndOfCurve(actualRotation)
+      );
+    }
+
+    for (let i = 0; i < arrayCurves.length; i++) {
+      let curvesClone = arrayCurves[i].clone();
+      curvesClone.scale(1, -1, view.center.add([this.circleSize, 0]));
+      arrayCurves[i].join(curvesClone);
+      arrayCurves[i].closePath();
+      arrayCurves[i].rotate(90, view.center);
+      arrayCurves[i].sendToBack();
+      arrayCurves[i].fillColor = this.colors[i];
+    }
+
+    arrayCurves[arrayCurves.length - 1].style = {
+      shadowColor: "rgba(21, 21, 21, .3)",
+      shadowBlur: 2,
+      shadowOffset: [0, 5]
+    };
+
+    circle.clone().fillColor = this.colors[0];
+
+    let shadowWidth = this.DATA_ARRAY[Math.ceil(this.DATA_ARRAY.length / 2)];
+    if (shadowWidth <= 1) shadowWidth = 1;
+
+    let circleShadow = new paper.Shape.Circle(view.center, 25 * Math.log(2 * shadowWidth));
+    circleShadow.sendToBack();
+    circleShadow.fillColor = {
+      origin: circleShadow.position,
+      destination: circleShadow.bounds.rightCenter,
+      gradient: {
+        stops: [[this.colors[1], shadowWidth / 300], ['transparent', 1]],
+        radial: true
+      }
+    };
+
+    circle.bringToFront();
+
+    circle.style = {
+      fillColor: "#fff",
+      shadowColor: "rgba(21, 21, 21, .3)",
+      shadowBlur: 2,
+      shadowOffset: [0, 5]
+    };
+
+    if (this.vibration && this.DATA_ARRAY.every(level => level > 50)) {
+      const randomX = this.vibration[0] - this.getRandomInt(0, this.vibration[0] * 2),
+        randomY = this.vibration[1] - this.getRandomInt(0, this.vibration[1] * 2);
+
+      circle.position = [view.center.x + randomX, view.center.y + randomY];
+      if (this.coverVibrates) {
+        this.cover.style.marginLeft = randomX+'px';
+        this.cover.style.marginTop = randomY+'px';
+      }
+    } else if (this.coverVibrates) {
+      this.cover.style.marginLeft = '0px';
+      this.cover.style.marginTop = '0px';
+    }
+  }
+
+  normalize = (current, inner) => Math.pow(current, 1 / (inner ? 2.25 : 2)) * 8;
+
+  getEndOfLine = (size, degrees) => view.center.add([(size + this.circleSize) * this.cos(degrees), (size + this.circleSize) * this.sin(degrees)])
+
+  getEndOfCurve = (currentAngle) => view.center.add([
+    this.cos(this.bisection(currentAngle)) * this.circleSize,
+    this.sin(this.bisection(currentAngle)) * this.circleSize
+  ])
+
+  bisection = (currentAngle) => currentAngle + this.DEGS_PER_LINE / 2
+
+  onResize = () => view.viewSize = new Size(window.innerWidth, window.innerHeight);
+
+  sin = number => this.roundNumber(Math.sin(this.toRadians(number)))
+
+  cos = number => this.roundNumber(Math.cos(this.toRadians(number)))
+
+  toRadians = degree => degree * (Math.PI / 180)
+
+  roundNumber = (number, decimals = 5) => Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals)
+
+  getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+}
--- components/Visualizer/Visualizer.jsx
+++ components/Visualizer/Visualizer.jsx
@@ -1,8 +1,6 @@
 import "./Visualizer.styl";
-import { paper } from "paper";
+import Visualizer from './Visualizer.js';
 import React from "react";
-import { pathToFileURL } from "url";
-import { magentaBright } from "ansi-colors";
 
 export default class extends React.Component {
   constructor(props) {
@@ -12,131 +10,30 @@ export default class extends React.Component {
 
     this.canvasRef = React.createRef();
     this.audioRef = React.createRef();
+    this.coverRef = React.createRef();
+
+    this.visualizer = new Visualizer(
+      this.audioRef,
+      this.canvasRef,
+      this.coverRef,
+      ["#1A1A1A", "#ff0050"],
+      100,
+      5,
+      null,
+      true
+    );
   }
 
   canvasRef;
   audioRef;
 
   componentDidMount() {
-    const SONG_PATH = "/static/Media/Sounds/song.mp3",
-      COLORS = ["#080808", "#ff0050"];
-    let audio = this.audioRef.current,
-      audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
-      analyser = audioCtx.createAnalyser(),
-      canvas = this.canvasRef.current;
+    this.visualizer.init();
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
 
-    audio.src = SONG_PATH;
-    audio.load();
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    // Sets the source to the media element & adds the analyser.
-    audioCtx.createMediaElementSource(audio).connect(analyser);
-
-    // links the analyser output to the speakers.
-    analyser.connect(audioCtx.destination);
-
-    //ammount of waveSamples (?)
-    analyser.fftSize = 2 ** 5;
-
-    const BUFFER_LENGTH = analyser.frequencyBinCount,
-      DATA_ARRAY = new Uint8Array(BUFFER_LENGTH),
-      //DEGS_PER_LINE = 180 / BUFFER_LENGTH,
-      DEGS_PER_LINE = 180 / 5;
-
-    paper.install(window);
-    paper.setup(canvas);
-
-    function renderFrame() {
-      requestAnimationFrame(renderFrame);
-      analyser.getByteFrequencyData(DATA_ARRAY);
-      project.activeLayer.removeChildren();
-
-      let lastBisection = view.center.add([100, 0]),
-        actualRotation = -DEGS_PER_LINE / 2,
-        arrayCurves = new Array(2).fill().map(() => new paper.Path()),
-        circle = new paper.Shape.Circle(view.center, 100);
-
-      for (let i = 6; i <= BUFFER_LENGTH; i++) {
-        if (i % 2) actualRotation += DEGS_PER_LINE;
-        arrayCurves[i % 2].moveTo(lastBisection);
-        arrayCurves[i % 2].quadraticCurveTo(
-          getEndOfLine(DATA_ARRAY[i], actualRotation),
-          (lastBisection = getEndOfCurve(actualRotation))
-        );
-      }
-      for (let i = 0; i < arrayCurves.length; i++) {
-        let curvesClone = arrayCurves[i].clone();
-        curvesClone.scale(1, -1, view.center.add([100, 0]));
-        arrayCurves[i].join(curvesClone);
-        arrayCurves[i].closePath();
-        arrayCurves[i].rotate(90, view.center);
-        arrayCurves[i].sendToBack();
-        arrayCurves[i].fillColor = COLORS[i];
-      }
-      arrayCurves[1].style = {
-        shadowColor: "rgba(21, 21, 21, .3)",
-        shadowBlur: 2,
-        shadowOffset: [0, 5]
-      };
-
-      circle.clone().fillColor = COLORS[0];
-
-      let shadowWidth = DATA_ARRAY[Math.ceil(DATA_ARRAY.length / 2)];
-      if (shadowWidth <= 1) shadowWidth = 1;
-      
-      let circleShadow = new paper.Shape.Circle(view.center, 25 * Math.log(2 * shadowWidth));
-      circleShadow.sendToBack();
-      circleShadow.fillColor = {
-        gradient: {
-            stops: [['#ff0050', shadowWidth / 300], ['transparent', 1]],
-            radial: true
-        },
-        origin: circleShadow.position,
-        destination: circleShadow.bounds.rightCenter
-    };
-      
-
-      circle.bringToFront();
-      circle.style = {
-        fillColor: "#fff",
-        shadowColor: "rgba(21, 21, 21, .3)",
-        shadowBlur: 2,
-        shadowOffset: [0, 5]
-      };
-
-      let canAnimate = true;
-      if (DATA_ARRAY.every(level => level > 50)) {
-        const x = view.center.x,
-          y = view.center.y;
-
-        circle.position = [
-          getRandomInt(x + 5, x - 5),
-          getRandomInt(y + 5, y - 5)
-        ];
-      }
-    }
-
-    renderFrame();
-
-    function getEndOfLine(size, degrees) {
-      return view.center.add([
-        (size + 100) * TRIGONOMETRY.cos(degrees),
-        (size + 100) * TRIGONOMETRY.sin(degrees)
-      ]);
-    }
-
-    function getEndOfCurve(currentAngle) {
-      return view.center.add([
-        TRIGONOMETRY.cos(bisection(currentAngle)) * 100,
-        TRIGONOMETRY.sin(bisection(currentAngle)) * 100
-      ]);
-    }
-
-    function bisection(currentAngle) {
-      return currentAngle + DEGS_PER_LINE / 2;
-    }
+  onResize() {
+    this.visualizer.onResize();
   }
 
   onCanvasClick() {
@@ -146,36 +43,24 @@ export default class extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="visualizer">
+        <div className="visualizer__cover" ref={this.coverRef}>
+          <img
+            className="visualizer__img"
+            src="/static/Media/Images/logo.png"
+            onClick={this.onCanvasClick.bind(this)}
+          />
+          {this.props.children}
+        </div>
         <canvas
-          id="oscilloscope"
+          className="visualizer__canvas"
           ref={this.canvasRef}
           onClick={this.onCanvasClick.bind(this)}
         />
-        <audio id="audio" controls ref={this.audioRef} />
-        <img
-          src="/static/Media/Images/logo.png"
-          onClick={this.onCanvasClick.bind(this)}
-        />
+        <audio className="visualizer__audio" controls ref={this.audioRef} src="/static/Media/Sounds/song2.mp3" />
+
       </div>
     );
   }
 }
 
-// Trigonometry101 for dummies:
-function toRadians(degree) {
-  return degree * (Math.PI / 180);
-}
-
-function roundNumber(number, decimals = 5) {
-  return Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals);
-}
-
-const TRIGONOMETRY = {
-  sin: number => roundNumber(Math.sin(toRadians(number))),
-  cos: number => roundNumber(Math.cos(toRadians(number)))
-};
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
